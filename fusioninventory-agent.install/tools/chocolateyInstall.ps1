80ecbb41d1a3d004c01f2cd89ea57722bb6aad8f
C-Duv <github.accounts@claude.duvergier.fr>
2016-08-11 02:20:52 +0200

    Use hash tables for Chocolatey install/download commands
    Install-ChocolateyPackage and Get-ChocolateyWebFile were called with a
    long list of parameters.
    This commit moves parameters into an hash table for better reading.

--- fusioninventory-agent.install/tools/chocolateyInstall.ps1
+++ fusioninventory-agent.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,10 @@
-$packageName = 'fusioninventory-agent.install'
-$installerType = 'exe'
-$url = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x86_2.3.18.exe'
-$url64 = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x64_2.3.18.exe'
-$silentArgs = '/S /acceptlicense'
-$validExitCodes = @(0)
+$packageInstallArgs = @{
+    packageName    = 'fusioninventory-agent.install'
+    fileType       = 'exe'
+    url            = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x86_2.3.18.exe'
+    url64bit       = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x64_2.3.18.exe'
+    silentArgs     = '/S /acceptlicense'
+    validExitCodes = @(0)
+}
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage @packageInstallArgs
--- fusioninventory-agent.portable/tools/chocolateyInstall.ps1
+++ fusioninventory-agent.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,10 @@
 $packageName = 'fusioninventory-agent.portable'
-$url = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x86_2.3.18-portable.exe' # NB: Theses EXE are 7z SFX
-$url64 = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x64_2.3.18-portable.exe'
+
+$packageDownloadArgs = @{
+    packageName = $packageName
+    url         = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x86_2.3.18-portable.exe' # NB: Theses EXE are 7z SFX
+    url64bit    = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x64_2.3.18-portable.exe'
+}
 
 try { 
   $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -11,7 +15,8 @@ try {
   if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
 
   $file = Join-Path $tempDir "$($packageName).exe" # Using ".exe" here to match extension of file located at $url/$url64
-  Get-ChocolateyWebFile "$packageName" "$file" "$url" "$url64"
+  $packageDownloadArgs.Add('FileFullPath', $file)
+  Get-ChocolateyWebFile @packageDownloadArgs
 
   Start-Process `"$file`" -ArgumentList "-o`"$installDir`" -y" -Wait # Downloaded file is a 7z SFX: just execute it
 
