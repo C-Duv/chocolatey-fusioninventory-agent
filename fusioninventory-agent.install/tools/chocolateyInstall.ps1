3c44f22986c8775a7b75a4a78ae4bbc26c2c6166
C-Duv <github.accounts@claude.duvergier.fr>
2016-08-11 02:32:12 +0200

    Adds checksums for downloaded remote files
    Chocolatey can perform checksum verifications on downloaded files (such as
    installers). They recently announced that checksums would be mandatory (date
    not yet determined).
    
    This commit adds SHA256 checksums of FusionInventory Agent installers.

--- fusioninventory-agent.install/tools/chocolateyInstall.ps1
+++ fusioninventory-agent.install/tools/chocolateyInstall.ps1
@@ -5,6 +5,10 @@ $packageInstallArgs = @{
     url64bit       = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x64_2.3.18.exe'
     silentArgs     = '/S /acceptlicense'
     validExitCodes = @(0)
+    checksum       = '5610705b44d14cb14f8d815a39526dc702eae359f6d64e560b8730bfd230164d'
+    checksumType   = 'sha256'
+    checksum64     = 'c901531de9c35e6af38b298a00bbc04acf258ebfcdd61a482d7a2a5d19566015'
+    checksumType64 = 'sha256'
 }
 
 Install-ChocolateyPackage @packageInstallArgs
--- fusioninventory-agent.portable/tools/chocolateyInstall.ps1
+++ fusioninventory-agent.portable/tools/chocolateyInstall.ps1
@@ -4,6 +4,10 @@ $packageDownloadArgs = @{
     packageName = $packageName
     url         = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x86_2.3.18-portable.exe' # NB: Theses EXE are 7z SFX
     url64bit    = 'https://github.com/tabad/fusioninventory-agent-windows-installer/releases/download/2.3.18/fusioninventory-agent_windows-x64_2.3.18-portable.exe'
+    checksum       = 'fa472f4831f28842e7130bd751d7d1f63242e71428021a908d46f7255c1c97c5'
+    checksumType   = 'sha256'
+    checksum64     = '07b09f73f06e520b09b5ca91cac291e915bb174f11b3bad420c8de9364d058b2'
+    checksumType64 = 'sha256'
 }
 
 try { 
