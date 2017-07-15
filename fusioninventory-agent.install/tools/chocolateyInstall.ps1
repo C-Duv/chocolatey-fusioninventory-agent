01c356256269948cb15ec9085631167cbd125fd6
C-Duv <github.accounts@claude.duvergier.fr>
2017-07-15 17:49:18 +0200

    Updates package to use agent v2.3.20
    This commit makes the package use FusionInventory Agent version 2.3.20.

--- fusioninventory-agent.install/tools/chocolateyInstall.ps1
+++ fusioninventory-agent.install/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 $packageInstallArgs = @{
     packageName    = 'fusioninventory-agent.install'
     fileType       = 'exe'
-    url            = 'https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.3.19/fusioninventory-agent_windows-x86_2.3.19.exe'
-    url64bit       = 'https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.3.19/fusioninventory-agent_windows-x64_2.3.19.exe'
+    url            = 'https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.3.20/fusioninventory-agent_windows-x86_2.3.20.exe'
+    url64bit       = 'https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.3.20/fusioninventory-agent_windows-x64_2.3.20.exe'
     silentArgs     = '/S /acceptlicense'
     validExitCodes = @(0)
-    checksum       = '31fef37543ccc0139e461577afc4c4154c35419d6700fc06e2d7aacf9b100e51'
+    checksum       = '9db73ac8aa779902219b13e834ec95009f6ac0015e743dfac1de52b6e6777a2c'
     checksumType   = 'sha256'
-    checksum64     = '8c53fe14d93e7bbf3d46b9d063702ae87997a619a568fd9524748883b446f092'
+    checksum64     = '20318a1ee5f3e2a61aa927b5c44032cf032d27dbcbc3e27b86ba2e51d7b96785'
     checksumType64 = 'sha256'
 }
 
