785d5db6cebc8313df02a3fec094966847a80ee3
C-Duv <1466273+C-Duv@users.noreply.github.com>
2018-07-14 20:20:45 +0200

    Updates package to use agent v2.4.1
    This commit makes the package use FusionInventory Agent version 2.4.1.

--- fusioninventory-agent.install/tools/chocolateyInstall.ps1
+++ fusioninventory-agent.install/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 $packageInstallArgs = @{
     packageName    = 'fusioninventory-agent.install'
     fileType       = 'exe'
-    url            = 'https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.4/fusioninventory-agent_windows-x86_2.4.exe'
-    url64bit       = 'https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.4/fusioninventory-agent_windows-x64_2.4.exe'
+    url            = 'https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.4.1/fusioninventory-agent_windows-x86_2.4.1.exe'
+    url64bit       = 'https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.4.1/fusioninventory-agent_windows-x64_2.4.1.exe'
     silentArgs     = '/S /acceptlicense'
     validExitCodes = @(0)
-    checksum       = '91796158da18bc53b11154ca9ac409b6915f9a7052caef5d671702ad2516ed32'
+    checksum       = '10987785b43496ee057aa1af57ec04e97b87962a23103835e79328652ce7ce4a'
     checksumType   = 'sha256'
-    checksum64     = '9a823580df4dd0cffa20d14c6266f1d05aeea789a4102f4026f98028912b49de'
+    checksum64     = '546a2e25bce5c5615dc32723be86effd464372801681bb644f4235e575598f77'
     checksumType64 = 'sha256'
 }
 
